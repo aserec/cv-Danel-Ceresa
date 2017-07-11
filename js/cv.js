84dcc66776bc9c8235421f1872bd99238f55e175
dabel <danel@damel.com>
2017-07-11 11:52:48 +0100

    i18n with english translations

--- js/cv.js
+++ js/cv.js
@@ -1,10 +1,16 @@
-// Freelancer Theme JavaScript
-
 (function($) {
     "use strict"; // Start of use strict
     var contestDone=false
 
-    var technologies=[
+    var i18n = window.domI18n({
+        selector: '[data-translatable]',
+        separator: ' // ',
+        languages: ['es-ES', 'en'],
+        defaultLanguage: 'es-ES'
+    });
+
+    var technologies = [];
+    technologies[0]=[
         ['HTML5', 80],
         ['CSS3', 45],
         ['Java', 85],
@@ -19,13 +25,38 @@
         ['Git', 95],
         ['Wordpress', 15]
     ];
-    var languages=[
+    technologies[1]=[
+        ['HTML5', 80],
+        ['CSS3', 45],
+        ['Java', 85],
+        ['Grails', 75],
+        ['Spring', 65],
+        ['Hibernate', 65],
+        ['PHP', 20],
+        ['MySql/Oracle', 55],
+        ['Cordova', 60],
+        ['Angular', 25],
+        ['Javascript', 70],
+        ['Git', 95],
+        ['Wordpress', 15]
+    ];
+
+    var languages = [];
+    languages[0]=[
         ['Euskera', 100],
         ['Ingles', 65],
         ['Catalan', 15],
         ['Italiano', 30]
     ];
-    var personality=[
+    languages[1]=[
+        ['Basque', 100],
+        ['Spanish', 100],
+        ['English', 65],
+        ['Catalan', 15],
+        ['Italian', 30]
+    ];
+    var personality = [];
+    personality[0]=[
         ['Dinamismo', 100],
         ['Alegria', 85],
         ['Humor', 85],
@@ -36,12 +67,31 @@
         ['Trabajo en equipo', 70],
         ['Sumision', 20]
     ];
-    var trasnsversals=[
+    personality[1]=[
+        ['Dynamism', 100],
+        ['Happyness', 85],
+        ['Houmor', 85],
+        ['Patience', 70],
+        ['Silence', 10],
+        ['Imagination', 60],
+        ['Respect', 95],
+        ['Teamwork', 85],
+        ['Summision', 20]
+    ];
+    var trasnsversals = [];
+    trasnsversals[0]=[
         ['Agile', 60],
         ['Tareas administrativas', 20],
         ['Formacion continua', 100],
-        ['Disponibilidad movimiento', 70],
-        ['Trato con cliente', 65]
+        ['Disponibilidad movimiento/traslado', 100],
+        ['Trato con cliente', 70]
+    ];
+    trasnsversals[1]=[
+        ['Agile', 60],
+        ['Administrative tasks', 20],
+        ['Continuous formation', 100],
+        ['Moving disposition', 100],
+        ['Client management', 70]
     ];
 
     $('.page-scroll a').bind('click', function(event) {
@@ -49,6 +99,7 @@
         if(contestDone){
             slowScroll($(this).attr('href'),event);
         }else{
+            i18n.changeLanguage(getNavigatorLanguage());
             askForContest($(this),event);
         }
     });
@@ -138,9 +189,8 @@
         }, 1250, 'easeInOutExpo');
         if(event!=null){event.preventDefault()};
     }
-
     function showChart(element){
-        var abilityArray=getAbilityArrayFromElementId(element[0].id);
+        var abilityArray=getAbilityArrayFromElementId(element[0].id, getLanguageIndex());
         var chart = c3.generate({
             bindto: '#'+element.children('.chartContainer')[0].id,
             data: {
@@ -158,15 +208,15 @@
             _recursiveChart(1,abilityArray)
         }, 1000);
 
-        function getAbilityArrayFromElementId(id){
+        function getAbilityArrayFromElementId(id, languageId){
             if(id==="other-technologies") {
-                return technologies;
+                return technologies[languageId];
             }else if (id==="other-lang"){
-                return languages;
+                return languages[languageId];
             }else if(id==="other-transversal"){
-                return trasnsversals;
+                return trasnsversals[languageId];
             }else{
-                return personality;
+                return personality[languageId];
             }
         }
 
@@ -185,6 +235,17 @@
                 }
             });
         }
+    }
+    function getLanguageIndex (){
+        if(window.navigator.language === "en-US" || window.navigator.language.indexOf("en") === 0 ){
+            return 1
+        }else{
+            return 0
+        }
+    }
+    function getNavigatorLanguage(){
+        return window.navigator.language;
+        // return 'en'
     }
 
 })(jQuery); // End of use strict
