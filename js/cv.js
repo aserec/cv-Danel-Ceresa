d255e2256694a1c48565c50fafef917780ea2b51
dabel <danel@damel.com>
2018-01-26 18:10:08 +0100

    Added Innofis experience

--- js/cv.js
+++ js/cv.js
@@ -1,6 +1,6 @@
-(function($) {
+(function ($) {
     "use strict";
-    var contestDone=false
+    var contestDone = false
 
     var i18n = window.domI18n({
         selector: '[data-translatable]',
@@ -11,12 +11,21 @@
 
     i18n.changeLanguage(getNavigatorLanguage());
 
-    $('.page-scroll a').bind('click', function(event) {
+    $('.page-scroll a').bind('click', function (event) {
         event.preventDefault();
-        if(contestDone){
-            slowScroll($(this).attr('href'),event);
-        }else{
-            askForContest($(this),event);
+        if (contestDone) {
+            slowScroll($(this).attr('href'), event);
+        } else {
+            askForContest($(this), event);
+        }
+    });
+
+    $('#profile').bind('click', function (event) {
+        event.preventDefault();
+        if (contestDone) {
+            slowScroll('#work', event);
+        } else {
+            askForContest($(this), event);
         }
     });
 
@@ -25,95 +34,100 @@
         offset: 51
     });
 
-    $('.navbar-collapse ul li a').click(function(){
-            $('.navbar-toggle:visible').click();
+    $('.navbar-collapse ul li a').click(function () {
+        $('.navbar-toggle:visible').click();
     });
 
     $('#mainNav').affix({
         offset: {
             top: 100
         }
-    })
-    $('#ok-image').click(function(){
-        $('.close-modal').trigger( "click" );
+    });
+
+    $('#ok-image').click(function () {
+        $('.close-modal').trigger("click");
         setTimeout(function () {
-            $('#contestModalLinkOk').trigger( "click" );
-        },500);
+            $('#contestModalLinkOk').trigger("click");
+        }, 500);
 
     });
 
-    $('#ko-image').click(function(){
+    $('#ko-image').click(function () {
         contestDo();
-        $('.close-modal').trigger( "click" );
-        slowScroll($(this).data('href'),event);
+        $('.close-modal').trigger("click");
+        slowScroll('#work', event);
     });
-    $('.fakeAnswer').click(function(){
+
+    $('.fakeAnswer').click(function () {
         $('#fakeQuestion').hide('slow');
         $('#contestQuestion').removeClass('hidden');
     });
-    $('#resolveBtn').click(function(event){
+    
+    $('#resolveBtn').click(function (event) {
         event.preventDefault();
-        var cousins= [$('#cousin1'),$('#cousin2'),$('#cousin3')];
-        if(validateForm(cousins)){
+        var cousins = [$('#cousin1'), $('#cousin2'), $('#cousin3')];
+        if (validateForm(cousins)) {
             showSuccessMessage();
             setTimeout(function () {
                 contestDo();
-                $('.close-modal').trigger( "click" );
-                slowScroll($('#ok-image').data('href'),event);
-            },1000);
+                $('.close-modal').trigger("click");
+                slowScroll($('#ok-image').data('href'), event);
+            }, 1000);
 
-        }else{
+        } else {
             showErrorMessage();
         }
     });
 
-    $('#other-lang, #other-transversal, #other-technologies, #other-personality').one('click', function() {
+    $('#other-lang, #other-transversal, #other-technologies, #other-personality').one('click', function () {
         showChart($(this));
     });
 
 
-    function validateForm(cousins){
-        if(cousins[0].val()==2 && cousins[1].val()==11 && cousins[2].val()==17){
+    function validateForm(cousins) {
+        if (cousins[0].val() == 2 && cousins[1].val() == 11 && cousins[2].val() == 17) {
             return true
         }
-        else if(cousins[0].val()==2 && cousins[1].val()==5 && cousins[2].val()==23){
+        else if (cousins[0].val() == 2 && cousins[1].val() == 5 && cousins[2].val() == 23) {
             return true
         }
     }
-    
-    function showSuccessMessage(){
+
+    function showSuccessMessage() {
         $('#wrongAnswer').hide();
         $('#goodAnswer').hide();
         $('#goodAnswer').fadeIn('slow');
     }
 
-    function showErrorMessage(){
+    function showErrorMessage() {
         $('#goodAnswer').hide();
         $('#wrongAnswer').hide();
         $('#wrongAnswer').fadeIn('slow');
     }
 
-    function askForContest(element,event){
-        $('#contestModalLink').trigger( "click" );
-        $('#ok-image').data('href',element.attr('href'));
-        $('#ko-image').data('href',element.attr('href'));
-
+    function askForContest(element, event) {
+        $('#contestModalLink').trigger("click");
+        $('#ok-image').data('href', element.attr('href'));
+        $('#ko-image').data('href', element.attr('href'));
     }
-    function contestDo(){
+
+    function contestDo() {
         window.scrollTo(0, 0);
         $('#secret').show();
-        contestDone=true;
+        contestDone = true;
     }
-    function slowScroll(href,event){
+
+    function slowScroll(href, event) {
         $('html, body').stop().animate({
             scrollTop: ($(href).offset().top - 50)
         }, 1250, 'easeInOutExpo');
-        if(event!=null){event.preventDefault()};
+        if (event != null) { event.preventDefault() };
     }
-    function showChart(element){
-        var abilityArray=getAbilityArrayFromElementId(element[0].id, getLanguageIndex());
+
+    function showChart(element) {
+        var abilityArray = getAbilityArrayFromElementId(element[0].id, getLanguageIndex());
         var chart = c3.generate({
-            bindto: '#'+element.children('.chartContainer')[0].id,
+            bindto: '#' + element.children('.chartContainer')[0].id,
             data: {
                 columns: [
                     abilityArray[0]
@@ -126,23 +140,23 @@
         });
 
         setTimeout(function () {
-            _recursiveChart(1,abilityArray)
+            _recursiveChart(1, abilityArray)
         }, 1000);
 
-        function getAbilityArrayFromElementId(id, languageId){
-            if(id==="other-technologies") {
+        function getAbilityArrayFromElementId(id, languageId) {
+            if (id === "other-technologies") {
                 return technologies[languageId];
-            }else if (id==="other-lang"){
+            } else if (id === "other-lang") {
                 return languages[languageId];
-            }else if(id==="other-transversal"){
+            } else if (id === "other-transversal") {
                 return trasnsversals[languageId];
-            }else{
+            } else {
                 return personality[languageId];
             }
         }
 
-        function _recursiveChart(index, abilityArray){
-            if(abilityArray[index]==null){
+        function _recursiveChart(index, abilityArray) {
+            if (abilityArray[index] == null) {
                 return false
             }
             chart.flow({
@@ -157,14 +171,16 @@
             });
         }
     }
-    function getLanguageIndex (){
-        if(window.navigator.language === "es" || window.navigator.language.indexOf("es") === 0 ){
+
+    function getLanguageIndex() {
+        if (window.navigator.language === "es" || window.navigator.language.indexOf("es") === 0) {
             return 0
-        }else{
+        } else {
             return 1
         }
     }
-    function getNavigatorLanguage(){
+
+    function getNavigatorLanguage() {
         return window.navigator.language;
     }
 
